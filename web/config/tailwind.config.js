8f8ab62525c61d661fb4af8bb090123d4ddb62cd
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-04-24 18:52:05 +1200

    Update TailwindCSS configuration and add new colors and fonts

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: '#314063',
+        white: '#fefefe',
+        greyViolet: '#B6B7C8',
+        blueGrey: '#738BAC',
+        slateBlue: '#535F93',
+        royalBlue: '#233277',
+        darkerGreyViolet: '#7E7F9A',
+        black: '#000000',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      gfs_didot_regular: ['GFS Didot', 'serif'],
+    },
   },
   plugins: [],
 }
-
