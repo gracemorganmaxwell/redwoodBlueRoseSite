9cf1299df56868b0f6ff8221172cf8425092ec0d
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-04-24 16:20:38 +1200

    Generated Nav Component

---
+++ web/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,10 @@
+const Navigation = () => {
+  return (
+    <div>
+      <h2>{'Navigation'}</h2>
+      <p>{'Find me in ./web/src/components/Navigation/Navigation.jsx'}</p>
+    </div>
+  )
+}
+
+export default Navigation
