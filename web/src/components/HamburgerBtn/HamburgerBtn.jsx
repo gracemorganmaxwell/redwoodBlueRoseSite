0ee31db6492a1fc8462a3ec6e5a63d60c63ee9a5
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-04-29 14:22:25 +1200

    Refactor HamburgerBtn component to use anchor tags for navigation, update styles, and reset default state for the hamburger btn image

--- web/src/components/HamburgerBtn/HamburgerBtn.jsx
+++ web/src/components/HamburgerBtn/HamburgerBtn.jsx
@@ -1,34 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import activeHamburger from 'public/images/activeHamburger.svg';
-import hamburger from 'public/images/hamburger.svg';
+import activeHamburger from 'public/images/activeHamburger.svg'
+import hamburger from 'public/images/hamburger.svg'
 
 const HamburgerBtn = () => {
-  const [isOpen, setIsOpen] = useState(false); // Controls the state of the menu
-  const toggleMenu = () => setIsOpen(!isOpen); // Toggles the state
+  const [isOpen, setIsOpen] = useState(false) // Controls the state of the menu
+  const toggleMenu = () => setIsOpen(!isOpen) // Toggles the state
 
   return (
-    <>
-      <div>
-        <button onClick={toggleMenu} aria-label="Menu button">
-          {isOpen ? (
-            <img src={activeHamburger} alt="Close menu button" /> // Show active hamburger when open
-          ) : (
-            <img src={hamburger} alt="Menu button" /> // Show default hamburger when closed
-          )}
-        </button>
-        {isOpen && (
-          <div className="rounded bg-greyViolet p-4 font-gfs_didot_regular text-lg text-royalBlue">
-            <a href="/#about">About</a>
-            <a href="/#treatments">Treatments</a>
-            <a href="/#reviews">Reviews</a>
-            <a href="/#gallery">Gallery</a>
-            <a href="/#book-now">Book Now</a>
-          </div>
+    <div className="relative">
+      <button
+        onClick={toggleMenu}
+        aria-label="Menu button"
+        className="flex h-20 w-20 items-center justify-center rounded-lg bg-darkBlue"
+      >
+        {isOpen ? (
+          <img
+            src={activeHamburger}
+            alt="Close menu button"
+            className="h-16 w-16"
+          /> // Show active hamburger when open
+        ) : (
+          <img src={hamburger} alt="Menu button" className="h-16 w-16" /> // Show default hamburger when closed
         )}
-      </div>
-    </>
-  );
+      </button>
+      {isOpen && (
+        <div className="absolute right-0 top-full mt-2 rounded bg-greyViolet font-gfs_didot_regular text-lg text-royalBlue shadow-lg">
+          <div className="flex flex-col space-y-2 p-4">
+            <a
+              href="/#about"
+              className="block rounded p-2 text-right text-xl font-bold transition duration-300 hover:bg-white hover:text-black"
+            >
+              About Me
+            </a>
+            <a
+              href="/#treatments"
+              className="block rounded p-2 text-right  text-xl font-bold transition duration-300 hover:bg-white hover:text-black"
+            >
+              Treatments
+            </a>
+            <a
+              href="/#reviews"
+              className="block rounded p-2 text-right text-xl font-bold transition duration-300 hover:bg-white hover:text-black"
+            >
+              Testimonies
+            </a>
+            <a
+              href="/#gallery"
+              className="block rounded p-2 text-right text-xl font-bold transition duration-300 hover:bg-white hover:text-black"
+            >
+              My Work
+            </a>
+            <a
+              href="/#book-now"
+              className="block rounded p-2 text-right  text-xl font-bold transition duration-300 hover:bg-white hover:text-black"
+            >
+              Book Now
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
-export default HamburgerBtn;
+export default HamburgerBtn
