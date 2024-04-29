83159ed6bd77d0670eda0210c4bbf14aa1a6faa4
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-04-29 13:46:14 +1200

    Refactor HamburgerBtn component to use anchor tags for navigation, reset default state for the hamburger btn image

--- web/src/components/HamburgerBtn/HamburgerBtn.jsx
+++ web/src/components/HamburgerBtn/HamburgerBtn.jsx
@@ -1,38 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import activeHamburger from 'public/images/activeHamburger.svg'
-import hamburger from 'public/images/hamburger.svg'
+import activeHamburger from 'public/images/activeHamburger.svg';
+import hamburger from 'public/images/hamburger.svg';
 
 const HamburgerBtn = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggleMenu = () => setIsOpen(!isOpen)
+  const [isOpen, setIsOpen] = useState(false); // Controls the state of the menu
+  const toggleMenu = () => setIsOpen(!isOpen); // Toggles the state
 
   return (
-    <div>
-      <button
-        className="menu-icon focus:outline-none"
-        onClick={toggleMenu}
-        aria-label="Menu button"
-      >
-        <div>
+    <>
+      <div>
+        <button onClick={toggleMenu} aria-label="Menu button">
           {isOpen ? (
-            <img src={hamburger} alt="Menu Button" />
+            <img src={activeHamburger} alt="Close menu button" /> // Show active hamburger when open
           ) : (
-            <img src={activeHamburger} alt="Open Menu Button" />
+            <img src={hamburger} alt="Menu button" /> // Show default hamburger when closed
           )}
-        </div>
-      </button>
-      {isOpen && (
-        <div>
-          <a href="/#about">About</a>
-          <a href="/#treatments">Treatments</a>
-          <a href="/#reviews">Reviews</a>
-          <a href="/#gallery">Gallery</a>
-          <a href="/#book-now">Book Now</a>
-        </div>
-      )}
-    </div>
-  )
+        </button>
+        {isOpen && (
+          <div className="rounded bg-greyViolet p-4 font-gfs_didot_regular text-lg text-royalBlue">
+            <a href="/#about">About</a>
+            <a href="/#treatments">Treatments</a>
+            <a href="/#reviews">Reviews</a>
+            <a href="/#gallery">Gallery</a>
+            <a href="/#book-now">Book Now</a>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default HamburgerBtn
+export default HamburgerBtn;
