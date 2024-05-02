681b47a6ccc48e55ee6bce8c9fb157970ca9018d
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-05-02 14:57:13 +1200

    Refactor CtaButton component to add click functionality and adjust button styles

--- web/src/components/CtaButton/CtaButton.jsx
+++ web/src/components/CtaButton/CtaButton.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import goldAssentBtnCorner from 'web/public/images/goldAssentBtnCorner.svg'
 
 const CtaButton = () => {
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClick = () => {
+    setIsClicked(true)
+    setTimeout(() => {
+      window.open('/#', '_blank')
+      setIsClicked(false)
+    }, 200)
+  }
+
   return (
     <div className="relative flex items-center justify-center">
       <img
@@ -15,7 +25,14 @@ const CtaButton = () => {
         alt="gold accents bottom"
         className="absolute bottom-0 left-0 ml-[55px] h-auto w-auto -translate-x-1/2 translate-y-full rotate-180 object-cover"
       />
-      <button className="text-cta relative z-10 flex items-center justify-center rounded-[12px] border border-darkBlue bg-white px-4 py-2 font-semibold leading-none text-darkBlue shadow-[0_4px_4px_rgba(0,0,0,0.80)] sm:px-6 sm:py-4 sm:text-xl md:min-w-[200px] md:px-8 md:py-5 md:text-2xl lg:min-w-[250px] lg:px-10 lg:py-6 lg:text-3xl xl:min-w-[300px] xl:px-12 xl:py-7 xl:text-4xl">
+      <button
+        className={`text-cta relative z-10 flex items-center justify-center rounded-[12px] border border-darkBlue px-4 py-2 font-semibold leading-none shadow-[0_4px_4px_rgba(0,0,0,0.80)] transition-colors duration-300 sm:px-6 sm:py-4 sm:text-xl md:min-w-[200px] md:px-8 md:py-5 md:text-2xl lg:min-w-[250px] lg:px-10 lg:py-6 lg:text-3xl xl:min-w-[300px] xl:px-12 xl:py-7 xl:text-4xl ${
+          isClicked
+            ? 'bg-darkBlue text-white'
+            : 'bg-white text-darkBlue hover:bg-darkBlue hover:text-white'
+        }`}
+        onClick={handleClick}
+      >
         Book Now
       </button>
     </div>
