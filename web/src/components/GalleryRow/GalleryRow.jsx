c88668e53f421ee7a0a7e856ad6bcdb298d9fc81
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-05-12 17:12:56 +1200

    Refactor GalleryRow component to add image carousel functionality

--- web/src/components/GalleryRow/GalleryRow.jsx
+++ web/src/components/GalleryRow/GalleryRow.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import headingAssent from 'web/public/images/headingAssent.svg'
+import './Gallery.css'
 
 const GalleryRow = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+  const images = [
+    { src: 'path/to/image1.jpg', alt: 'Image 1 description' },
+    { src: 'path/to/image2.jpg', alt: 'Image 2 description' },
+    { src: 'path/to/image3.jpg', alt: 'Image 3 description' },
+    // Add more image objects as needed
+  ]
+
+  const previousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    )
+  }
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    )
+  }
+
   return (
     <div className="bg-darkBlue py-16 sm:py-24 md:py-32">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -23,6 +45,35 @@ const GalleryRow = () => {
             alt=""
             className="lg:w-100 mx-auto mb-6 mt-6 h-auto w-auto sm:w-60 md:w-80"
           />
+          {/* My Work Examples Carousel */}
+          <div className="gallery">
+            <div className="gallery-container">
+              <img
+                src={images[currentImageIndex].src}
+                alt={images[currentImageIndex].alt}
+                className="gallery-image shadow-polaroid"
+              />
+              <div className="gallery-controls">
+                <button
+                  className="gallery-control-button"
+                  onClick={previousImage}
+                >
+                  &lt;
+                </button>
+                <div className="gallery-image-info">
+                  <span className="gallery-image-index">
+                    {currentImageIndex + 1} / {images.length}
+                  </span>
+                  <span className="gallery-image-alt">
+                    {images[currentImageIndex].alt}
+                  </span>
+                </div>
+                <button className="gallery-control-button" onClick={nextImage}>
+                  &gt;
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
