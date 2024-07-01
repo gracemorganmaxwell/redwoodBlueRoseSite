cf4dc698b5f92142835d39b26a2b8dafd07d62d2
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-07-02 11:03:40 +1200

    fixed image sizing in the About component

--- web/src/components/AboutRow/AboutRow.jsx
+++ web/src/components/AboutRow/AboutRow.jsx
@@ -28,9 +28,9 @@ const AboutRow = () => {
           {/* About content */}
           <div className="mt-8 flex flex-col sm:gap-2 md:gap-4 lg:gap-6 xl:gap-8">
             {/* Row 1 */}
-            <div className="flex flex-row items-center justify-between gap-4">
+            <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
               {/* Box 1 */}
-              <p className="sm:text text-center text-base sm:text-left md:text-xl lg:text-2xl">
+              <p className="text-center text-base sm:text-left md:text-xl lg:text-2xl">
                 Welcome to Blue Rose Nails and Beauty, where your journey to
                 serenity and elegance begins.
               </p>
@@ -38,27 +38,27 @@ const AboutRow = () => {
               <img
                 src={aboutImg2}
                 alt="Anna, sitting at a workstation, doing a client's nails in her home based beauty salon."
-                className="h-[117.6px] w-[162.4px] sm:h-[117.6px] sm:w-[162.4px] md:h-[283.36px] md:w-[391.31px] lg:h-[391.46px] lg:w-[540.58px]"
+                className="h-[157.6px] w-[202.4px] object-cover md:h-[283.36px] md:w-[391.31px] lg:h-[391.46px] lg:w-[540.58px]"
               />
             </div>
             {/* Row 2 */}
-            <div className="flex flex-row items-center gap-4 sm:gap-2">
+            <div className="flex flex-col-reverse items-center gap-4 sm:flex-row sm:gap-2">
               {/* Box 3 */}
               <img
                 src={aboutImg1}
                 alt="Anna, a close up of her face, smiling at the camera."
-                className="h-[232.4px] w-[150px] sm:h-[232.4px] sm:w-[150px] md:h-[302.57px] md:w-[200px] lg:h-[519.55px] lg:w-[300px]"
+                className="h-[322.57px] w-[220px] object-cover md:h-[302.57px] md:w-[200px] lg:h-[519.55px] lg:w-[300px]"
               />
               {/* Box 4 */}
-              <p className="text-center text-base sm:mr-2 sm:text-right md:text-xl lg:text-2xl">
+              <p className="text-center text-base sm:mr-2 sm:py-3 sm:text-right md:text-xl lg:text-2xl">
                 I&apos;m Anna, with over five years of passion for transforming
                 everyday stress into moments of beauty and relaxation.
               </p>
             </div>
             {/* Row 3 */}
-            <div className="flex flex-row items-center gap-4 sm:gap-2">
+            <div className="flex flex-col items-center gap-4 sm:flex-row sm:gap-2">
               {/* Box 5 */}
-              <p className="text-center text-base sm:text-left md:text-xl lg:text-2xl">
+              <p className="text-center text-base sm:py-3 sm:text-left md:text-xl lg:text-2xl">
                 Dive into a personalized pampering experience in our tranquil
                 haven, designed to rejuvenate your spirit and enhance your
                 natural beauty.
@@ -67,7 +67,7 @@ const AboutRow = () => {
               <img
                 src={aboutImg3}
                 alt="A reflection in a mirror of a client getting their eyebrows done on a massage table."
-                className="h-[153px] w-[161px]  sm:h-[153px] sm:w-[161px] md:h-[229.11px] md:w-[196.11px] lg:h-[451.96px] lg:w-[386.86px]"
+                className="h-[229.11px] w-[196.11px] object-cover md:h-[229.11px] md:w-[196.11px] lg:h-[451.96px] lg:w-[386.86px]"
               />
             </div>
           </div>
