bbe46e7f63386ef566f951501fcdb6ffb48f12e1
grace-morganmaxwell <gracemorganmaxwell@gmail.com>
2024-07-16 18:20:59 +1200

    updates to the form pages

--- web/src/pages/ContactPage/ContactPage.jsx
+++ web/src/pages/ContactPage/ContactPage.jsx
@@ -21,15 +21,27 @@ const ContactPage = () => {
 
   return (
     <main>
-      <div className="overflow-x-hidden">
+      <div className="w-full overflow-x-hidden bg-darkBlue">
         {isLoading ? (
           <LoadingScreen />
         ) : (
           <>
             <Metadata title="contact" description="contact" />
             <NavigationComponent />
-            <h1>Contact Us</h1>
-            <ContactFormComponent />
+            <div className="mx-auto max-w-2xl text-center">
+              <p className="my-4 font-sans text-white">
+                Thank you for reaching out to us! We value your feedback and
+                inquiries. Please fill out the contact form below with your
+                name, email, and message. Our team is committed to providing
+                timely responses and will get back to you as soon as possible.
+                Whether you have questions, comments, or need assistance, we are
+                here to help. Your input is important to us, and we strive to
+                ensure a positive experience with our services.
+              </p>
+              <div className="shadow-xl">
+                <ContactFormComponent />
+              </div>
+            </div>
             <FooterComponent />
           </>
         )}
--- web/src/pages/GiftRequestPage/GiftRequestPage.jsx
+++ web/src/pages/GiftRequestPage/GiftRequestPage.jsx
@@ -1,18 +1,56 @@
-import { React } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Metadata } from '@redwoodjs/web'
 
+import Footer from 'src/components/Footer/Footer'
 import GiftCardRequestComponent from 'src/components/GiftCardRequestComponent/GiftCardRequestComponent'
+import LoadingScreen from 'src/components/LoadingSpinner/LoadingSpinner'
 import NavigationComponent from 'src/components/Navigation/Navigation'
 
 const GiftRequestPage = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    // Simulating a loading delay for demonstration
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+
+    return () => clearTimeout(timer) // Cleanup the timer
+  }, [])
+
   return (
-    <div>
-      <Metadata title="Gift Request" description="Gift Requests" />
-      <NavigationComponent />
-      <h1>Gift Card Requests Page</h1>
-      <GiftCardRequestComponent />
-    </div>
+    <>
+      <div className="w-full overflow-x-hidden bg-darkBlue">
+        {isLoading ? (
+          <LoadingScreen />
+        ) : (
+          <>
+            <Metadata
+              title="Gift Request"
+              description="Gift card requests form"
+            />
+            <NavigationComponent />
+            <div className="mx-auto max-w-2xl text-center">
+              <p className="my-4 font-sans text-white">
+                Welcome to our Gift Card Request page! Here, you can easily
+                request a gift card for your loved ones or yourself. Simply fill
+                out the form below with the recipient's details, your preferred
+                gift type, and the delivery method. Whether you choose an e-gift
+                card or a physical gift card, we ensure a seamless and prompt
+                process. If you have any special messages or instructions,
+                please include them in the form. Thank you for choosing our gift
+                cards to make someone's day special!
+              </p>
+              <div className="w-full bg-darkBlue p-6 shadow-lg">
+                <GiftCardRequestComponent />
+              </div>
+              <Footer />
+            </div>
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
