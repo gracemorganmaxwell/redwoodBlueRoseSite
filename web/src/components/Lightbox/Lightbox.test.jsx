e62ac7b84898594146156405543175d86464518d
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-07-31 20:15:20 +1200

    Fixed the lightbox test

--- web/src/components/Lightbox/Lightbox.test.jsx
+++ web/src/components/Lightbox/Lightbox.test.jsx
@@ -1,29 +1,53 @@
+import { Cloudinary } from '@cloudinary/url-gen'
+
 import { render } from '@redwoodjs/testing/web'
 
 import Lightbox from './Lightbox'
 
-//   Improve this test with help from the Redwood Testing Doc:
-//    https://redwoodjs.com/docs/testing#testing-components
-
 describe('Lightbox', () => {
   it('renders successfully', () => {
-    const mockImages = [
-      { src: 'image1.jpg', alt: 'Image 1' },
-      { src: 'image2.jpg', alt: 'Image 2' },
+    const mockMediaItems = [
+      {
+        publicId: 'Gel_manicure_with_coloured_French_qurqfv',
+        type: 'image',
+        alt: 'Gel manicure with coloured french tips.',
+      },
+      {
+        publicId: 'Lash_lift_and_hybrid_brow_stain_zf3e33',
+        type: 'video',
+        alt: 'Lash lift and hybrid brow stain.',
+      },
+      {
+        publicId: 'Soft_gel_extensions_with_a_foil_feature_nail_mbjdcc',
+        type: 'image',
+        alt: 'Soft gel extensions with a foil feature nail.',
+      },
+      {
+        publicId: 'Lash_lift_and_hybrid_brow_stain_zf3e33',
+        type: 'video',
+        alt: 'Video 2',
+      }
     ]
-    const mockCurrentImageIndex = 0
-    const mockPreviousImage = jest.fn()
-    const mockNextImage = jest.fn()
+    const mockCurrentMediaIndex = 0
+    const mockPreviousMedia = jest.fn()
+    const mockNextMedia = jest.fn()
     const mockCloseLightbox = jest.fn()
+
+    const cld = new Cloudinary({
+      cloud: {
+        cloudName: 'dioi9wxj7',
+      },
+    })
 
     expect(() => {
       render(
         <Lightbox
-          images={mockImages}
-          currentImageIndex={mockCurrentImageIndex}
-          previousImage={mockPreviousImage}
-          nextImage={mockNextImage}
+          mediaItems={mockMediaItems}
+          currentMediaIndex={mockCurrentMediaIndex}
+          previousMedia={mockPreviousMedia}
+          nextMedia={mockNextMedia}
           closeLightbox={mockCloseLightbox}
+          cld={cld} // Pass the Cloudinary instance
         />
       )
     }).not.toThrow()
