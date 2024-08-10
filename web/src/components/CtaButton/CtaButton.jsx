acb98e3312b0aaa213919b6e779d35764f526334
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2024-08-10 12:31:54 +1200

    feat: Add hover effect to CTA button with white background, darkBlue text, and border

--- web/src/components/CtaButton/CtaButton.jsx
+++ web/src/components/CtaButton/CtaButton.jsx
@@ -5,7 +5,7 @@ import goldAssentBtnCorner from 'web/public/images/goldAssentBtnCorner.svg'
 const CtaButton = () => {
   const handleClick = () => {
     window.open(
-      ' https://www.fresha.com/book-now/blue-rose-nails-beauty-h5gamcsb/all-offer?pId=969545',
+      'https://www.fresha.com/book-now/blue-rose-nails-beauty-h5gamcsb/all-offer?pId=969545',
       '_blank'
     )
   }
@@ -18,7 +18,7 @@ const CtaButton = () => {
         className="absolute right-0 top-0 mr-[-20px] mt-[-10px] h-auto w-auto object-cover"
       />
       <button
-        className="font- relative z-10 flex items-center justify-center rounded-[12px] border border-darkBlue bg-darkBlue px-6 py-3 font-sans text-base font-bold text-white shadow-md transition duration-300 hover:bg-ascentBlue hover:text-white md:text-lg lg:text-xl"
+        className="relative z-10 flex items-center justify-center rounded-[12px] border border-darkBlue bg-darkBlue px-6 py-3 font-sans text-base font-bold text-white shadow-md transition duration-300 hover:border-darkBlue hover:bg-white hover:text-darkBlue md:text-lg lg:text-xl"
         onClick={handleClick}
       >
         Book Now
